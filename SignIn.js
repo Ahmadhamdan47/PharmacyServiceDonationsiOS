b327053b28509a5e6dfc0307799cbe716f29d985
Ahmadhamdan47 <ahmadhamdan47@gmail.com>
2026-02-03 23:56:05 +0200

    fix: update version info to v3.0.15 (13) in SignIn component

--- SignIn.js
+++ SignIn.js
@@ -558,7 +558,7 @@ const SignIn = () => {
         )}
         
         {/* Version Info */}
-        <Text style={styles.versionText}>v3.0.15 (12) - Feb 04, 2026</Text>
+        <Text style={styles.versionText}>v3.0.15 (13) - Feb 04, 2026</Text>
       </View>
         </ScrollView>
       </TouchableWithoutFeedback>
